fb8ae3d6e0cc38e2e8e88728e9e281e820dc522e
Ashif Rahman <ashifrahman8638471722@gmail.com>
2025-07-10 19:58:28 +0530

    mobile-view updated

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const container = document.getElementById('container');
 const signUpBtn = document.getElementById('signUp');
 const signInBtn = document.getElementById('signIn');
+const loginLink = document.querySelector('.login');
+const signupLink = document.querySelector('.signup');
+
 signUpBtn.addEventListener('click', () => {
   container.classList.add("right-panel-active");
 });
+
 signInBtn.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
+});
+
+loginLink.addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    container.classList.add('show-login');
+    container.classList.remove('show-signup');
+  } else {
+    container.classList.add('right-panel-active');
+  }
+});
+
+signupLink.addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    container.classList.add('show-signup');
+    container.classList.remove('show-login');
+  } else {
+    container.classList.remove('right-panel-active');
+  }
 });
